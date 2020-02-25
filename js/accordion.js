350dca21a2ce4f330f1e9d8ba66fbd57e4c0de61
miguelbnmv <7222miguel@gmail.com>
2020-02-25 21:11:36 +0000

    Improve code

--- js/accordion.js
+++ js/accordion.js
@@ -1,12 +1,8 @@
 const items = document.querySelectorAll(".accordion a");
-console.log(items)
 
 function toggleAccordion(){
-console.log('um');
-this.classList.toggle('active');
-this.nextElementSibling.classList.toggle('active');
-
-console.log(this)
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
 }
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
--- js/index1.js
+++
@@ -1,4 +0,0 @@
-function myFunction() {
-  const menuToggle = document.getElementById("ola");
-  console.log(menuToggle);
-  menuToggle.setAttribute("checked", "checked");
