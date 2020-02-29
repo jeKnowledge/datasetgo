5ab7f5e74a1f0fcb7fa9d70dd78f685863960c37
mariamachado6 <molimpiadias@gmail.com>
2020-02-29 15:33:09 +0000

    Fix images, partners, menu

---
+++ js/navbar.js
@@ -0,0 +1,33 @@
+$( "#option1" ).click(function() {
+  $('.menu-section').removeClass('on');
+  $('.menu-toggle').removeClass('on');
+  $('.menu').addClass('hidden');
+});
+
+
+$( "#option2" ).click(function() {
+  $('.menu-section').removeClass('on');
+  $('.menu-toggle').removeClass('on');
+  $('.menu').addClass('hidden');
+});
+
+
+$( "#option3" ).click(function() {
+  $('.menu-section').removeClass('on');
+  $('.menu-toggle').removeClass('on');
+  $('.menu').addClass('hidden');
+});
+
+
+$( "#option4" ).click(function() {
+  $('.menu-section').removeClass('on');
+  $('.menu-toggle').removeClass('on');
+  $('.menu').addClass('hidden');
+});
+
+
+$( "#option5" ).click(function() {
+  $('.menu-section').removeClass('on');
+  $('.menu-toggle').removeClass('on');
+  $('.menu').addClass('hidden');
+});
