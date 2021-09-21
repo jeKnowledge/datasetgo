51ce69778f691475f67dd9a4bf3744398ee67c60
Polpy <emailparaoedu@gmail.com>
2021-09-21 17:12:29 +0100

    faq logic

--- js/accordion.js
+++ js/accordion.js
@@ -1,1 +1,10 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    console.log(this);
+    //var acc = document.getElementsByClassName("accordion");
+    this.classList.toggle("active");
+  });
+}
